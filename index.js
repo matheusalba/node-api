b1c441289594ff1f6a1550cfa3b437745b6c0a51
matheusalba <matheuscampos306@gmail.com>
2026-06-03 15:44:20 -0300

    porta

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3000;
-
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 
 const pool = require('./db');
 
